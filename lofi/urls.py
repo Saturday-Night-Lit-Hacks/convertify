528d98ef5330e3abdcf0ae203d4b7a464e424aed
Cindy Gu <cindygu@umich.edu>
2020-10-24 15:06:51 -0400

    Added requesting a lofi song page

--- lofi/urls.py
+++ lofi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('request', views.request_lofi, name='request_lofi')
 ]
