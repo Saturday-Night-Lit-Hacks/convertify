157bd3126878f1c20638018bcd83eef4450896f8
tanyalai <tanyalai@umich.edu>
2020-10-24 13:36:38 -0400

    Base files

--- lofi/views.py
+++ lofi/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    context = {'name': 'Tanya Lai'}
+    if request.method == 'POST':
+        data = request.POST
+        context['name'] = data.get('firstname')
+        
+    return render(request, 'index.html', context)
+
+def input(request):
+    return 1
+
+
+def scrape(request):
+    return 1
