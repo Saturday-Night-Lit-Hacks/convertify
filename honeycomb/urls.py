d15f684b4cab7d7a94c3d4356067ca85c4a05a6e
Himaja Motheram <hmothera@umich.edu>
2020-10-24 13:01:22 -0400

    Add basic index page

--- honeycomb/urls.py
+++ honeycomb/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+# from lofi import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', include("lofi.urls")),
 ]
