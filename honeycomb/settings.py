f0cace19dfea4eed6931ee802e14aa9f494210f6
Cindy Gu <cindygu@umich.edu>
2020-10-25 10:23:43 -0400

    Trying a fix

--- honeycomb/settings.py
+++ honeycomb/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
